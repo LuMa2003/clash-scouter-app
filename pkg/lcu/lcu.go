d95e0be5c18056300073d61995ab63ff7a3720a8
Lucas Mårtensson <39569742+LuMa2003@users.noreply.github.com>
2024-07-31 23:35:55 +0200

    Created lcu wrapper and auth retriever
    feat: Add LCU package for interacting with LeagueClientUx
    
    - Implement ConnInfo struct to store port and auth information
    - Implement Request struct to store request details
    - Add GetAuth function to retrieve authentication information from running LeagueClientUx process
    - Add LCU function to make HTTP requests to the LeagueClientUx API
    
    The GetAuth function uses wmic to extract the command line arguments of the LeagueClientUx process and retrieves the app port and remoting auth token. These values are encoded in base64 and returned in a ConnInfo struct.
    
    The LCU function takes a Request struct, builds an HTTP request with the appropriate headers, and executes it, returning the response body.

---
+++ pkg/lcu/lcu.go
@@ -0,0 +1,61 @@
+package lcu
+
+import (
+	"bytes"
+	b64 "encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os/exec"
+	"regexp"
+	"strconv"
+)
+
+type ConnInfo struct {
+	Port uint16
+	Auth string
+}
+
+type Request struct {
+	connInfo *ConnInfo
+	method   string
+	endpoint string
+	body     io.Reader
+}
+
+func GetAuth() (ConnInfo, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("wmic", "PROCESS", "WHERE", "name='LeagueClientUx.exe'", "GET", "commandline")
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+	if stderr.Len() != 0 {
+		return ConnInfo{}, errors.New("League of Legends is not running")
+	}
+
+	port, _ := strconv.Atoi(string(regexp.MustCompile(`--app-port=([0-9]*)`).FindAllSubmatch(stdout.Bytes(), 1)[0][1]))
+	token := string(regexp.MustCompile(`--remoting-auth-token=([\w-_]*)`).FindAllSubmatch(stdout.Bytes(), 1)[0][1])
+
+	return ConnInfo{Port: uint16(port), Auth: b64.StdEncoding.EncodeToString([]byte("riot:" + token))}, nil
+}
+
+func LCU(request *Request) ([]byte, error) {
+	requestURL := fmt.Sprint("https://127.0.0.1:", request.connInfo.Port, request.endpoint)
+	req, err := http.NewRequest(request.method, requestURL, request.body)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Authorization", "Basic "+request.connInfo.Auth)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil || res.StatusCode != 200 {
+		return nil, errors.New("request failed")
+	}
+	resBody, err := io.ReadAll(res.Body)
+
+	return resBody, nil
+}
