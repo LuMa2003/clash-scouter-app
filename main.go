c92e41c1296b3aa160c8ed45cc76247768e53d5e
Lucas Mårtensson <39569742+LuMa2003@users.noreply.github.com>
2024-08-01 19:52:44 +0200

    Get the region of the current user

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"github.com/LuMa2003/clash-scouter-app/pkg/lcu"
+	"github.com/tidwall/gjson"
 	"net/http"
 	"time"
 )
@@ -17,6 +19,22 @@ func main() {
 	start := time.Now()
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	connInfo, err := lcu.GetAuth()
+
+	check(err)
+
+	data, err := lcu.LCU(lcu.Request{
+		Conn:     &connInfo,
+		Method:   "GET",
+		Endpoint: "/riotclient/region-locale",
+		Body:     nil,
+	})
+	check(err)
+
+	region := gjson.GetBytes(data, "region").String()
+
+	fmt.Println(region)
 
 	duration := time.Since(start)
 	fmt.Println(duration)
