fab181e62f91ce28cd3903f63f6be8a4ac3d9fae
Lucas Mårtensson <39569742+LuMa2003@users.noreply.github.com>
2024-08-01 19:28:19 +0200

    Cleared main.go

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"crypto/tls"
-	b64 "encoding/base64"
 	"fmt"
-	"github.com/LuMa2003/clash-scouter-app/internal"
-	"github.com/tidwall/gjson"
-	"io"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -20,45 +13,10 @@ func check(e error) {
 	}
 }
 
-func getAuth() (uint16, string) {
-	dat, err := os.ReadFile("C:/Riot Games/League of Legends/lockfile")
-	check(err)
-	lockfile := strings.Split(string(dat), ":")
-
-	port, _ := strconv.Atoi(lockfile[2])
-
-	auth := b64.StdEncoding.EncodeToString([]byte("riot:" + lockfile[3]))
-
-	return uint16(port), auth
-}
-
 func main() {
 	start := time.Now()
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-
-	port, auth := getAuth()
-
-	fmt.Printf("Port: %v Auth %v \n", port, auth)
-
-	requestURL := fmt.Sprintf("https://127.0.0.1:%d/lol-summoner/v1/current-summoner", port)
-	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
-	check(err)
-	req.Header.Add("Authorization", "Basic "+auth)
-
-	res, err := client.Do(req)
-	check(err)
-
-	fmt.Println(res.StatusCode)
-
-	resBody, err := io.ReadAll(res.Body)
-	check(err)
-
-	Summoners_array := gjson.GetManyBytes(resBody, "gameName", "tagLine")
-	cli.Cli(fmt.Sprint(Summoners_array[0].Value(), "-", Summoners_array[1].Value()))
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
 	duration := time.Since(start)
 	fmt.Println(duration)
