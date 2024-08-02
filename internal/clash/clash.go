3709664fb69fb26407f34bf9dd865801d0ca030d
Kladdiskakan <kevingregard@gmail.com>
2024-08-02 14:55:17 +0200

    Made clash function loops
    First for not finding a tournament bracket.
    The other for not finding an opponenet

--- internal/clash/clash.go
+++ internal/clash/clash.go
@@ -3,6 +3,10 @@ package clash
 import (
 	"github.com/LuMa2003/clash-scouter-app/pkg/lcu"
 	"github.com/tidwall/gjson"
+	"github.com/manifoldco/promptui"
+	"fmt"
+	"os"
+
 )
 
 type Summoner struct {
@@ -25,61 +29,117 @@ func ClashOpponent(connInfo *lcu.ConnInfo) ([]Summoner, error) {
 	me := summary[0].Int()
 	bracketId := summary[1].String()
 
+
+	for { 
 	data2, err := lcu.LCU(lcu.Request{
 		Conn:     connInfo,
 		Method:   "GET",
 		Endpoint: "/lol-clash/v1/bracket/" + bracketId,
 		Body:     nil,
 	})
-	if err != nil {
-		return nil, err
+	
+	if err == nil{
+
+		bracket := gjson.GetManyBytes(data2, `matches.#(status=="UPCOMING")#.rosterId1`, `matches.#(status=="UPCOMING")#.rosterId2`)
+		roster1 := &bracket[0]
+		roster2 := &bracket[1]
+
+		var opponent string
+
+		for index := range roster1.Indexes {
+			if roster1.Array()[index].Int() == me {
+				opponent = roster2.Array()[index].String()
+			} else if roster2.Array()[index].Int() == me {
+				opponent = roster1.Array()[index].String()
+			}
+		}
+	//ERROR HÄR PGA Ingen opponenet
+		data3, err := lcu.LCU(lcu.Request{
+			Conn:     connInfo,
+			Method:   "GET",
+			Endpoint: "/lol-clash/v1/roster/" + opponent,
+			Body:     nil,
+		})
+
+		if err == nil {	
+			roster := gjson.GetBytes(data3, "members.#.summonerId").String()
+
+			data4, err := lcu.LCU(lcu.Request{
+				Conn:     connInfo,
+				Method:   "GET",
+				Endpoint: "/lol-summoner/v2/summoners/?ids=" + roster,
+				Body:     nil,
+			})
+
+			if err != nil {
+				return nil, err
+			}
+			summonersJson := gjson.GetManyBytes(data4, "#.gameName", "#.tagLine")
+			name := &summonersJson[0]
+			tag := &summonersJson[1]
+
+			var summoners []Summoner
+
+			for index := range name.Indexes {
+				summoners = append(summoners, Summoner{
+					Name: name.Array()[index].String(),
+					Tag:  tag.Array()[index].String(),
+				})
+			}
+			return summoners, nil
+		}
+		
+		//Om Opponent inte hittas
+		if err != nil{
+			prompt := promptui.Select{
+				Label:    "Opponent not found",
+				HideHelp: true,
+				Size:     10,
+				Items:    []string{"Retry","Exit"},
+			}
+			_, result, err := prompt.Run()
+		
+			if err != nil {
+				fmt.Printf("Prompt failed %v\n", err)
+				continue
+			}
+			if result == "Exit" {
+				fmt.Println("Exiting...")
+				os.Exit(0)
+			}
+		}
+
 	}
-
-	bracket := gjson.GetManyBytes(data2, `matches.#(status=="UPCOMING")#.rosterId1`, `matches.#(status=="UPCOMING")#.rosterId2`)
-	roster1 := &bracket[0]
-	roster2 := &bracket[1]
-
-	var opponent string
-
-	for index := range roster1.Indexes {
-		if roster1.Array()[index].Int() == me {
-			opponent = roster2.Array()[index].String()
-		} else if roster2.Array()[index].Int() == me {
-			opponent = roster1.Array()[index].String()
+	//Om Clash Bracket inte hittas
+	if err != nil{
+		prompt := promptui.Select{
+			Label:    "Clashbracket not found",
+			HideHelp: true,
+			Size:     10,
+			Items:    []string{"Retry","Exit"},
+		}
+		_, result, err := prompt.Run()
+	
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			continue
+		}
+		if result == "Exit" {
+			fmt.Println("Exiting...")
+			os.Exit(0)
 		}
 	}
 
-	data3, err := lcu.LCU(lcu.Request{
-		Conn:     connInfo,
-		Method:   "GET",
-		Endpoint: "/lol-clash/v1/roster/" + opponent,
-		Body:     nil,
-	})
-	if err != nil {
-		return nil, err
 	}
-	roster := gjson.GetBytes(data3, "members.#.summonerId").String()
 
-	data4, err := lcu.LCU(lcu.Request{
-		Conn:     connInfo,
-		Method:   "GET",
-		Endpoint: "/lol-summoner/v2/summoners/?ids=" + roster,
-		Body:     nil,
-	})
-	if err != nil {
-		return nil, err
-	}
-	summonersJson := gjson.GetManyBytes(data4, "#.gameName", "#.tagLine")
-	name := &summonersJson[0]
-	tag := &summonersJson[1]
-
-	var summoners []Summoner
-
-	for index := range name.Indexes {
-		summoners = append(summoners, Summoner{
-			Name: name.Array()[index].String(),
-			Tag:  tag.Array()[index].String(),
-		})
-	}
-	return summoners, nil
 }
+
+
+
+
+
+
+
+
+
+
--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/LuMa2003/clash-scouter-app/pkg/lcu"
-	"github.com/LuMa2003/clash-scouter-app/internal/cli"
 	"github.com/LuMa2003/clash-scouter-app/internal/clash"
+	"github.com/LuMa2003/clash-scouter-app/internal/cli"
+	"github.com/LuMa2003/clash-scouter-app/pkg/lcu"
+	"github.com/manifoldco/promptui"
 	"github.com/tidwall/gjson"
 	"net/http"
 	"time"
-	"github.com/manifoldco/promptui"
-
 )
 
 func check(e error) {
@@ -19,18 +18,15 @@ func check(e error) {
 	}
 }
 
-
 func main() {
 	start := time.Now()
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
-
 	for {
 		connInfo, err := lcu.GetAuth()
-	
+
 		if err == nil {
-			// If no error, print success and break out of the loop
 			data, err := lcu.LCU(lcu.Request{
 				Conn:     &connInfo,
 				Method:   "GET",
@@ -38,28 +34,23 @@ func main() {
 				Body:     nil,
 			})
 			check(err)
-		
 			region := gjson.GetBytes(data, "region").String()
-		
-		
+
 			summoner_array, err := clash.ClashOpponent(&connInfo)
-		
-			
+			fmt.Println(err)
 			cli.Cli(&summoner_array, region)
 			break
 		}
 
-		
-	if err != nil{
 		prompt := promptui.Select{
-			Label:    "Något gick fel",
+			Label:    err,
 			HideHelp: true,
 			Size:     10,
-			Items:    []string{"Retry","Exit"},
+			Items:    []string{"Retry", "Exit"},
 		}
 
 		_, result, err := prompt.Run()
-	
+
 		if err != nil {
 			fmt.Printf("Prompt failed %v\n", err)
 			return
@@ -67,11 +58,9 @@ func main() {
 		if result == "Exit" {
 			fmt.Println("Exiting...")
 			return
-		}	
-	
-	
+		}
+
 	}
-}
 	duration := time.Since(start)
 	fmt.Println(duration)
 }
